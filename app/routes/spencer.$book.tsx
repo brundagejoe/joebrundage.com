b3564f7b3dde941b7b33cad922c155a88ac45602
brundagejoe <joebrundage@icloud.com>
2023-09-04 15:37:51 -0600

    file restructure

--- app/routes/spencer.$book.tsx
+++ app/routes/spencer.$book.tsx
@@ -2,15 +2,15 @@ import Markdoc from "@markdoc/markdoc";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node"; // or cloudflare/deno
 import { useLoaderData } from "@remix-run/react";
-import * as fs from "fs/promises";
+import { promises as fs } from "fs";
 import React from "react";
 import { components, config } from "~/spencer/markdoc/Config";
 import { requirePassword } from "~/utils/session.server";
-import path from "path";
 
 export const loader = async ({ request }: LoaderArgs) => {
   await requirePassword(request);
-  const file = path.join(process.cwd(), "content/spencer", "1-nephi.markdoc");
+  // const file = path.join(process.cwd(), "content/spencer", "1-nephi.markdoc");
+  const file = __dirname + "/../content/spencer/1-nephi.markdoc";
   const fileContent = await fs.readFile(file, "utf-8");
   const ast = Markdoc.parse(fileContent);
 
